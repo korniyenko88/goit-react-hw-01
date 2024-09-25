bc3d2d577e68156d12b33d4177052b72d211db2a
unknown <you@example.com>
2024-09-25 11:47:57 +0300

    add comp FriendListItem

---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,18 @@
+import styles from "./FriendListItem.module.css";
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <div>
+      <img src={avatar} alt="Avatar" width="48" />
+      <p className={styles.friendstext}>{name}</p>
+      <p
+        className={styles.friendstext}
+        style={{ color: isOnline ? "green" : "red" }}
+      >
+        {isOnline ? "Online" : "Offline"}
+      </p>
+    </div>
+  );
+};
+
+export default FriendListItem;
--- src/components/FriendsList/FriendList.jsx
+++ src/components/FriendsList/FriendList.jsx
@@ -1,4 +1,4 @@
-import FriendListItem from "./FriendListItem";
+import FriendListItem from "../FriendListItem/FriendListItem";
 import styles from "./FriendsList.module.css";
 
 const FriendList = ({ friends }) => {
